6fc0ed2f35f93fc0a9130dd2044b9288b56c8112
betapcode17 <164189973+betapcode17@users.noreply.github.com>
2024-09-30 14:23:17 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,27 @@
-#include <iostream>
-#include "list.h"
+#include "linkedlist.h"
 #include "Book.h"
-#include <string>
-#include <conio.h>
-#include <fstream>
-#include <Windows.h>
+#include "Users.h"
+#include "HeThong.h"
+#include "menu.h"
 LinkedList listBook;
+LinkedList listCustomer;
+Users users;
 using namespace std;
 void Start()
 {
     listBook.readBook("books.txt");
-    listBook.displayBooks();
+    //listBook.displayBooks(5);
+    listCustomer.readCustomer("customers.txt");
 }
+/*void Process(){
+	int login = users.login();
+	system("cls");
+	cout<<login<<endl;
+	cout<<users.getRole();
+}*/
 int main() {
-     system("cls");
-    Start();
+	//Start();
+	//Process();
+	//ShowMenu()
+	ShowMenu();
 }
